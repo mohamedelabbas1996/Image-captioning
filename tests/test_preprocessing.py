de070293aa44e2299c0272b9b244ee077fa343ae
mohamedelabbas1996 <hack1996man@gmail.com>
2022-08-23 12:07:58 +0000

    added test

---
+++ tests/test_preprocessing.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestPreprocessing(unittest.TestCase):
+    def test_create_image_id(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
